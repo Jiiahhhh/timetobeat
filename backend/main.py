c99eb7006fb8ef85fb2f7716f39e975825abe935
DESKTOP-MOUI7DH\Pongo <ijlalnurhadi01@gmail.com>
2026-04-19 15:57:07 +0800

    update CORS origins and custom favicon

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,17 @@ from routers import recommendations
 
 app = FastAPI(title="TimeToBeat API", version="1.0.0")
 
+origins = [
+    "http://localhost:3000",
+    "https://timetobeat.app",           
+    "https://www.timetobeat.app",       
+    "https://timetobeat-saz7.vercel.app"
+]
+
 # Allow frontend (Next.js) to call backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://timetobeat-saz7.vercel.app",
-    ],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
